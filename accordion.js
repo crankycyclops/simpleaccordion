386b27c284f6ea536b4057d01056826e800e1ef9
James Colannino <crankycyclops@gmail.com>
2015-05-20 15:35:56 -0700

    initial import

---
+++ accordion.js
@@ -0,0 +1,20 @@
+/**
+ * Simple jQuery Accordion that doesn't reply on the extremely heavy jQuery UI
+ * library.
+ *
+ * Based on code from: http://uniondesign.ca/simple-accordion-without-jquery-ui/
+ */
+$.fn.SimpleAccordion = function(options) {
+
+	// open the default item (if any)
+	this.find('.accordion-item.default').find('.accordion-content').slideToggle('fast');
+
+	this.find('.accordion-toggle').click(function() {
+
+		//Expand or collapse this panel
+		$(this).next().slideToggle('fast');
+
+		//Hide the other panels
+		$('.accordion-content').not($(this).next()).slideUp('fast');
+	});
+}
