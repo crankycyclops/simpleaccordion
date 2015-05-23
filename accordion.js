afad8e66eb09355e4960b7c14b0818d2b0f085f8
FACFINDER <facfinder@hawaii-facfinder-test.(none)>
2015-05-22 18:06:59 -0700

    using  classes intead of data for keeping track of active tabs

--- accordion.js
+++ accordion.js
@@ -11,7 +11,7 @@ $.fn.SimpleAccordion = function(options) {
 
 	// open the default item (if any)
 	this.find('.accordion-item.default').find('.accordion-content').slideToggle('fast');
-	this.find('.accordion-item.default').data('active', true);
+	this.find('.accordion-item.default').addClass('active');
 
 	// if we have any default items that are active by default, we should call their callbacks
 	if (this.find('.accordion-item.default').length > 0) {
@@ -24,15 +24,19 @@ $.fn.SimpleAccordion = function(options) {
 
 		//Expand or collapse this panel
 		$(this).next().slideToggle('fast');
-		$(this).parent().data('active', $(this).parent().data('active') ? false : true);
+		if ($(this).parent().hasClass('active')) {
+			$(this).parent().removeClass('active');
+		} else {
+			$(this).parent().addClass('active');
+		}
 
 		//Hide the other panels
 		$('.accordion-content').not($(this).next()).slideUp('fast');
-		$('.accordion-content').not($(this).next()).parent().data('active', false);
+		$('.accordion-content').not($(this).next()).parent().removeClass('active');
 
 		// call callback (if one was specified)
 		if ('function' == typeof options.callback) {
-			options.callback($(this).parent().get(0), $(this).parent().data('active'));
+			options.callback($(this).parent().get(0), $(this).parent().hasClass('active'));
 		}
 	});
 }
